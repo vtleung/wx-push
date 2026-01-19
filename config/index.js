2f87db645058882e15840735d1544399868c26f2
vtleung <921714193@qq.com>
2026-01-20 01:53:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx00b26cd3fdaf1e69",
+  appsecret: "d6c7686487372338753f8bb9bf0ce91c",
+  template_id: "9mBUY9pHVKU76MW8U6XKgzU87u0R5MkbFYtYtn9sHEQ",
+  touser: "obayZ3Fsuuoy7NSwDbfIsvf-hqQM",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "邢台",
